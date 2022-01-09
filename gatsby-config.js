6f830801b879b4157f926db7e814207035abebf9
dimiandre <dimiandre@gmail.com>
2022-01-08 23:20:01 -0500

    add ipfs plugin, meta tags

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,11 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "NETA Money",
+    siteUrl: "https://neta.money",
+    title: "NETA Money | Decentralized Store of Value",
   },
+  pathPrefix: "__GATSBY_IPFS_PATH_PREFIX__",
   plugins: [
+    "gatsby-plugin-ipfs",
     "gatsby-plugin-styled-components",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
